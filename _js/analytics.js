99c1747d6be5ee17dad7a7b5464635bc528d412a
Luke Chesser <luke.chesser@me.com>
2015-08-15 16:33:58 -0400

    Fix focus CSS for navigation tab; update Crew link

--- _js/analytics.js
+++ _js/analytics.js
@@ -4,13 +4,11 @@
 	$(function () {
 		cache = {
 			$socialLinks: $('.js-social-link'),
-			$toCrew: $('.js-to-crew'),
 			$toRepo: $('.js-to-repo'),
 			$entryLink: $('.js-entry-link'),
 		};
 
 		analytics.trackLink(cache.$socialLinks, 'Clicked social link');
-		analytics.trackLink(cache.$toCrew, 'To Crew');
 		analytics.trackLink(cache.$toRepo, 'To repo');
 		analytics.trackLink(cache.$entryLink, function (target) {
 			return $(target).data('name');
